d4b624829221ddb503b219b042cf946bc98179bd
manojkandan1996 <manojkandan1996@gmail.com>
2025-09-01 09:21:50 +0530

    Update App.jsx

--- RE-Day1 & 2/profile-card-app/src/App.jsx
+++ RE-Day1 & 2/profile-card-app/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <main>
       <h1 style={{ textAlign: "center" }}>Profile Card App</h1>
-      <ProfileCard name="Vanishree" age="24" location="Bangalore" />
+      <ProfileCard name="manoj" age="29" location="tamil nadu" />
       <ProfileCard name="John Doe" age="30" location="New York" />
     </main>
   );
